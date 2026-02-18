27f7707a2143939f2d5edbd00d437f5ae36fc7d5
Demian <demanzakaluzhnij@gmail.com>
2026-02-18 13:47:03 +0200

    Demian's changes

--- BookListApi/BookListApi/settings.py
+++ BookListApi/BookListApi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'api_1',
     'api_2',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +129,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [    
     'rest_framework.authentication.SessionAuthentication',    
-    'rest_framework.authentication.BasicAuthentication',    
+    'rest_framework.authentication.BasicAuthentication',
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
 ],
 }
+
+SIMPLE_JWT = {"ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),}
--- BookListApi/api_2/urls.py
+++ BookListApi/api_2/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from rest_framework import urls
 # from views import BookModelVset
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 router = routers.DefaultRouter()
 router.register(r'books', views.BookModelVset)
@@ -12,4 +17,8 @@ urlpatterns = [
     # path('books/<int:pk>', views.book_info),
     path('genres/<int:pk>', views.genre_info, name="genre-detail"),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+
